61cd72a609aa8c39f5a29beb1b003cc63b19b086
I_En <102521991+laptopoutofbattery@users.noreply.github.com>
2022-04-15 17:48:44 +0800

    Create Basic RSA Encryption.py

---
+++ Basic RSA Encryption.py
@@ -0,0 +1,21 @@
+from Crypto.Util.number import getPrime
+from Crypto.Util.number import bytes_to_long
+import gmpy2
+
+#finding prime numbers and computing n
+p = getPrime(512)
+q = getPrime(512)
+n = p*q
+
+#compute phi and choose e
+phi = (p - 1) * (q - 1)
+e = 65537 #any number from 1 to phi that is coprime to phi (usually 65537)
+
+#compute d (private key exponent, used for decryption)
+d = int(gmpy2.invert(e, phi))
+
+#encrypt plaintext
+M = open("pt.txt", "rb").read() #plaintext message
+m = bytes_to_long(M)
+c = pow(m, e, n)
+print(c)
